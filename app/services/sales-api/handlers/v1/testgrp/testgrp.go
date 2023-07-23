0b1c15a1b4423b1ebee9c7028f9ff56b1664200c
Sumit Kumar Prasad <prasad28sumit@gmail.com>
2023-07-23 07:55:58 +0530

    Add shutdown validation

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,13 +2,19 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
+	v1Web "github.com/sum28it/garage-service/business/web/v1"
 	"github.com/sum28it/garage-service/foundation/web"
 )
 
 // Status represents a test handler for now.
 func Status(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return v1Web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+	}
 	status := struct {
 		Status string
 	}{
