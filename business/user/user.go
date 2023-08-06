db4753142849820b3785fd3f9bd1da6b79dddac7
Sumit Kumar Prasad <prasad28sumit@gmail.com>
2023-08-06 23:52:38 +0530

    Add user models

---
+++ business/user/user.go
@@ -0,0 +1,45 @@
+// Package user provides an example of a core business API. Right now these
+// calls are just wrapping the data/data layer. But at some point you will
+// want auditing or something that isn't specific to the data/store layer.
+package user
+
+import (
+	"context"
+	"errors"
+	"net/mail"
+
+	"github.com/google/uuid"
+)
+
+// Set of error variables for CRUD operations.
+var (
+	ErrNotFound              = errors.New("user not found")
+	ErrInvalidEmail          = errors.New("email is not valid")
+	ErrUniqueEmail           = errors.New("email is not unique")
+	ErrAuthenticationFailure = errors.New("authentication failed")
+	ErrInvalidOrder          = errors.New("validating order by")
+)
+
+// Storer interface declares the behavior this package needs to perists and
+// retrieve data.
+type Storer interface {
+	WithinTran(ctx context.Context, fn func(s Storer) error) error
+	Create(ctx context.Context, usr User) error
+	Update(ctx context.Context, usr User) error
+	Delete(ctx context.Context, usr User) error
+	Query(ctx context.Context, pageNumber int, rowsPerPage int) ([]User, error)
+	QueryByID(ctx context.Context, userID uuid.UUID) (User, error)
+	QueryByEmail(ctx context.Context, email mail.Address) (User, error)
+}
+
+// Core manages the set of APIs for user access.
+type Core struct {
+	storer Storer
+}
+
+// NewCore constructs a core for user api access.
+func NewCore(storer Storer) *Core {
+	return &Core{
+		storer: storer,
+	}
+}
