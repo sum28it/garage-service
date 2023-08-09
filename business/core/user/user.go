a92bd7e43ecaf8e83cef22ab36f3d05e257d2d77
Sumit Kumar Prasad <prasad28sumit@gmail.com>
2023-08-09 20:04:44 +0530

    Add test for user

--- business/core/user/user.go
+++ business/core/user/user.go
@@ -1,14 +1,18 @@
 // Package user provides an example of a core business API. Right now these
-// calls are just wrapping the data/data layer. But at some point you will
+// calls are just wrapping the data/data layer. But at some point we will
 // want auditing or something that isn't specific to the data/store layer.
 package user
 
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/mail"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/sum28it/garage-service/business/sys/validate"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Set of error variables for CRUD operations.
@@ -42,4 +46,132 @@ func NewCore(storer Storer) *Core {
 	return &Core{
 		storer: storer,
 	}
+}
+
+// Create inserts a new user into the database.
+func (c *Core) Create(ctx context.Context, nu NewUser) (User, error) {
+	if err := validate.Check(nu); err != nil {
+		return User{}, fmt.Errorf("validating data: %w", err)
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(nu.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, fmt.Errorf("generating password hash: %w", err)
+	}
+
+	now := time.Now()
+
+	usr := User{
+		ID:           uuid.New(),
+		Name:         nu.Name,
+		Email:        nu.Email,
+		PasswordHash: hash,
+		Roles:        nu.Roles,
+		Enabled:      true,
+		DateCreated:  now,
+		DateUpdated:  now,
+	}
+
+	// This provides an example of how to execute a transaction if required.
+	tran := func(s Storer) error {
+		if err := s.Create(ctx, usr); err != nil {
+			return fmt.Errorf("create: %w", err)
+		}
+		return nil
+	}
+
+	if err := c.storer.WithinTran(ctx, tran); err != nil {
+		return User{}, fmt.Errorf("tran: %w", err)
+	}
+
+	return usr, nil
+}
+
+// Update replaces a user document in the database.
+func (c *Core) Update(ctx context.Context, usr User, uu UpdateUser) (User, error) {
+	if err := validate.Check(uu); err != nil {
+		return User{}, fmt.Errorf("validating data: %w", err)
+	}
+
+	if uu.Name != nil {
+		usr.Name = *uu.Name
+	}
+	if uu.Email != nil {
+		usr.Email = *uu.Email
+	}
+	if uu.Roles != nil {
+		usr.Roles = uu.Roles
+	}
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return User{}, fmt.Errorf("generating password hash: %w", err)
+		}
+		usr.PasswordHash = pw
+	}
+	if uu.Enabled != nil {
+		usr.Enabled = *uu.Enabled
+	}
+	usr.DateUpdated = time.Now()
+
+	if err := c.storer.Update(ctx, usr); err != nil {
+		return User{}, fmt.Errorf("update: %w", err)
+	}
+
+	return usr, nil
+}
+
+// Delete removes a user from the database.
+func (c *Core) Delete(ctx context.Context, usr User) error {
+	if err := c.storer.Delete(ctx, usr); err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+
+	return nil
+}
+
+// Query retrieves a list of existing users from the database.
+func (c *Core) Query(ctx context.Context, pageNumber int, rowsPerPage int) ([]User, error) {
+	users, err := c.storer.Query(ctx, pageNumber, rowsPerPage)
+	if err != nil {
+		return nil, fmt.Errorf("query: %w", err)
+	}
+
+	return users, nil
+}
+
+// QueryByID gets the specified user from the database.
+func (c *Core) QueryByID(ctx context.Context, userID uuid.UUID) (User, error) {
+	user, err := c.storer.QueryByID(ctx, userID)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	return user, nil
+}
+
+// QueryByEmail gets the specified user from the database by email.
+func (c *Core) QueryByEmail(ctx context.Context, email mail.Address) (User, error) {
+	user, err := c.storer.QueryByEmail(ctx, email)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	return user, nil
+}
+
+// Authenticate finds a user by their email and verifies their password. On
+// success it returns a Claims User representing this user. The claims can be
+// used to generate a token for future authentication.
+func (c *Core) Authenticate(ctx context.Context, email mail.Address, password string) (User, error) {
+	usr, err := c.storer.QueryByEmail(ctx, email)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(usr.PasswordHash, []byte(password)); err != nil {
+		return User{}, ErrAuthenticationFailure
+	}
+
+	return usr, nil
 }
---
+++ business/core/user/user_test.go
@@ -0,0 +1,226 @@
+package user_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/mail"
+	"runtime/debug"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/sum28it/garage-service/business/core/user"
+	"github.com/sum28it/garage-service/business/core/user/stores/userdb"
+	"github.com/sum28it/garage-service/business/data/dbtest"
+	"github.com/sum28it/garage-service/foundation/docker"
+)
+
+var c *docker.Container
+
+func TestMain(m *testing.M) {
+	var err error
+	c, err = dbtest.StartDB()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer dbtest.StopDB(c)
+
+	m.Run()
+}
+
+func Test_User(t *testing.T) {
+	log, db, teardown := dbtest.NewUnit(t, c, "testuser")
+	defer func() {
+		if r := recover(); r != nil {
+			t.Log(r)
+			t.Error(string(debug.Stack()))
+		}
+		teardown()
+	}()
+
+	core := user.NewCore(userdb.NewStore(log, db))
+
+	t.Log("Given the need to work with User records.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen handling a single User.", testID)
+		{
+			ctx := context.Background()
+
+			email, err := mail.ParseAddress("test@example.com")
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to parse email: %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to parse email.", dbtest.Success, testID)
+
+			nu := user.NewUser{
+				Name:            "Test Gopher",
+				Email:           *email,
+				Roles:           []string{user.RoleAdmin},
+				Password:        "gophers",
+				PasswordConfirm: "gophers",
+			}
+
+			usr, err := core.Create(ctx, nu)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to create user : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to create user.", dbtest.Success, testID)
+
+			saved, err := core.QueryByID(ctx, usr.ID)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user by ID: %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve user by ID.", dbtest.Success, testID)
+
+			if usr.DateCreated.UnixMilli() != saved.DateCreated.UnixMilli() {
+				t.Logf("\t\tTest %d:\tGot: %v", testID, saved.DateCreated)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, usr.DateCreated)
+				t.Logf("\t\tTest %d:\tDiff: %v", testID, saved.DateCreated.Sub(usr.DateCreated))
+				t.Fatalf("\t%s\tTest %d:\tShould get back the same date created.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get back the same date created.", dbtest.Success, testID)
+
+			if usr.DateUpdated.UnixMilli() != saved.DateUpdated.UnixMilli() {
+				t.Logf("\t\tTest %d:\tGot: %v", testID, saved.DateUpdated)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, usr.DateUpdated)
+				t.Logf("\t\tTest %d:\tDiff: %v", testID, saved.DateUpdated.Sub(usr.DateUpdated))
+				t.Fatalf("\t%s\tTest %d:\tShould get back the same date updated.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get back the same date updated.", dbtest.Success, testID)
+
+			usr.DateCreated = time.Time{}
+			usr.DateUpdated = time.Time{}
+			saved.DateCreated = time.Time{}
+			saved.DateUpdated = time.Time{}
+
+			if diff := cmp.Diff(usr, saved); diff != "" {
+				t.Fatalf("\t%s\tTest %d:\tShould get back the same user. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get back the same user.", dbtest.Success, testID)
+
+			email, err = mail.ParseAddress("test@sheep.com")
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to parse email: %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to parse email.", dbtest.Success, testID)
+
+			upd := user.UpdateUser{
+				Name:     dbtest.StringPointer("Test Sheep"),
+				Email:    email,
+				Password: dbtest.StringPointer("newpassword"),
+			}
+
+			if _, err := core.Update(ctx, saved, upd); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to update user : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to update user.", dbtest.Success, testID)
+
+			saved, err = core.QueryByEmail(ctx, *upd.Email)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user by Email : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve user by Email.", dbtest.Success, testID)
+
+			diff := usr.DateUpdated.Sub(saved.DateUpdated)
+			if diff > 0 {
+				t.Fatalf("Should have a larger DateUpdated : sav %v, usr %v, dif %v", saved.DateUpdated, usr.DateUpdated, diff)
+			}
+
+			if saved.Name != *upd.Name {
+				t.Errorf("\t%s\tTest %d:\tShould be able to see updates to Name.", dbtest.Failed, testID)
+				t.Logf("\t\tTest %d:\tGot: %v", testID, saved.Name)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, *upd.Name)
+			} else {
+				t.Logf("\t%s\tTest %d:\tShould be able to see updates to Name.", dbtest.Success, testID)
+			}
+
+			if saved.Email != *upd.Email {
+				t.Errorf("\t%s\tTest %d:\tShould be able to see updates to Email.", dbtest.Failed, testID)
+				t.Logf("\t\tTest %d:\tGot: %v", testID, saved.Email)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, *upd.Email)
+			} else {
+				t.Logf("\t%s\tTest %d:\tShould be able to see updates to Email.", dbtest.Success, testID)
+			}
+
+			if err := core.Delete(ctx, saved); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to delete user : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to delete user.", dbtest.Success, testID)
+
+			_, err = core.QueryByID(ctx, saved.ID)
+			if !errors.Is(err, user.ErrNotFound) {
+				t.Fatalf("\t%s\tTest %d:\tShould NOT be able to retrieve user : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould NOT be able to retrieve user.", dbtest.Success, testID)
+		}
+	}
+}
+
+func Test_PagingUser(t *testing.T) {
+	log, db, teardown := dbtest.NewUnit(t, c, "testpaging")
+	defer func() {
+		if r := recover(); r != nil {
+			t.Log(r)
+			t.Error(string(debug.Stack()))
+		}
+		teardown()
+	}()
+
+	core := user.NewCore(userdb.NewStore(log, db))
+
+	t.Log("Given the need to page through User records.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen paging through 2 users.", testID)
+		{
+			ctx := context.Background()
+
+			name := "User Gopher"
+			users1, err := core.Query(ctx, 1, 1)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user %q : %s.", dbtest.Failed, testID, name, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve user %q.", dbtest.Success, testID, name)
+
+			if len(users1) != 1 && users1[0].Name == name {
+				t.Fatalf("\t%s\tTest %d:\tShould have a single user for %q : %s.", dbtest.Failed, testID, name, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould have a single user.", dbtest.Success, testID)
+
+			name = "Admin Gopher"
+			users2, err := core.Query(ctx, 1, 1)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user %q : %s.", dbtest.Failed, testID, name, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve users %q.", dbtest.Success, testID, name)
+
+			if len(users2) != 1 && users2[0].Name == name {
+				t.Fatalf("\t%s\tTest %d:\tShould have a single user for %q : %s.", dbtest.Failed, testID, name, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould have a single user.", dbtest.Success, testID)
+
+			users3, err := core.Query(ctx, 1, 2)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve 2 users for page 1 : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve 2 users for page 1.", dbtest.Success, testID)
+
+			if len(users3) != 2 {
+				t.Logf("\t\tTest %d:\tgot: %v", testID, len(users3))
+				t.Logf("\t\tTest %d:\texp: %v", testID, 2)
+				t.Fatalf("\t%s\tTest %d:\tShould have 2 users for page 1 : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould have 2 users for page 1.", dbtest.Success, testID)
+
+			if users3[0].ID == users3[1].ID {
+				t.Logf("\t\tTest %d:\tUser1: %v", testID, users3[0].ID)
+				t.Logf("\t\tTest %d:\tUser2: %v", testID, users3[1].ID)
+				t.Fatalf("\t%s\tTest %d:\tShould have different users : %s.", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould have different users.", dbtest.Success, testID)
+		}
+	}
+}
